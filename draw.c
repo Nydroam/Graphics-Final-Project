94d35badd32f55528c3d3ffee77b9c682aa700fb
Raymond Wu <rayw.u17@gmail.com>
2016-05-23 13:23:50 -0400

    Removed Extraneous Commenting

--- draw.c
+++ draw.c
@@ -178,68 +178,6 @@ void draw_polygons( struct matrix *polygons, screen s, color c ) {
 
   jdyrlandweaver
   ====================*/
-/*void add_sphere( struct matrix * points, 
-		 double cx, double cy, double cz, double r, 
-		 int step ) {
-
-  struct matrix * temp;
-  int lat, longt;
-  int index;
-  int num_steps, num_points;
-  double px0, px1, px2, px3;
-  double py0, py1, py2, py3;
-  double pz0, pz1, pz2, pz3;
-
-  num_steps = MAX_STEPS / step;
-  num_points = num_steps * (num_steps + 1);
-  
-  temp = new_matrix( 4, num_points);
-  //generate the points on the sphere
-  generate_sphere( temp, cx, cy, cz, r, step );
-
-  int latStop, longStop, latStart, longStart;
-  latStart = 0;
-  latStop = num_steps;
-  longStart = 0;
-  longStop = num_steps;
-
-  num_steps++;
-
-  for ( lat = latStart; lat < latStop; lat++ ) {
-    for ( longt = longStart; longt < longStop; longt++ ) {
-      
-      index = lat * num_steps + longt;
-
-      px0 = temp->m[0][ index ];
-      py0 = temp->m[1][ index ];
-      pz0 = temp->m[2][ index ];
-      
-      px1 = temp->m[0][ (index + num_steps) % num_points ];
-      py1 = temp->m[1][ (index + num_steps) % num_points ];
-      pz1 = temp->m[2][ (index + num_steps) % num_points ];
-
-      px3 = temp->m[0][ index + 1 ];
-      py3 = temp->m[1][ index + 1 ];
-      pz3 = temp->m[2][ index + 1 ];
-
-      if (longt != longStop - 1) {
-	px2 = temp->m[0][ (index + num_steps + 1) % num_points ];
-	py2 = temp->m[1][ (index + num_steps + 1) % num_points ];
-	pz2 = temp->m[2][ (index + num_steps + 1) % num_points ];
-      }
-      else {
-	px2 = temp->m[0][ (index + 1) % num_points ];
-	py2 = temp->m[1][ (index + 1) % num_points ];
-	pz2 = temp->m[2][ (index + 1) % num_points ];
-      }
-
-      if (longt != 0)
-	add_polygon( points, px0, py0, pz0, px1, py1, pz1, px2, py2, pz2 );
-      if (longt != longStop - 1)
-	add_polygon( points, px2, py2, pz2, px3, py3, pz3, px0, py0, pz0 );
-    }
-  }
-}*/
 
 void add_sphere( struct matrix * points, 
 		 double cx, double cy, double cz, double r, 
@@ -315,34 +253,6 @@ void add_sphere( struct matrix * points,
   03/22/12 11:30:26
   jdyrlandweaver
   ====================*/
-/*void generate_sphere( struct matrix * points, 
-		      double cx, double cy, double cz, double r, 
-		      int step ) {
-
-
-  int circle, rotation;
-  double x, y, z, circ, rot;
-
-  int rotStart = step * 0;
-  int rotStop = MAX_STEPS;
-  int circStart = step * 0;
-  int circStop = MAX_STEPS;
-  
-  for ( rotation = rotStart; rotation < rotStop; rotation += step ) {
-    rot = (double)rotation / MAX_STEPS;
-    for ( circle = circStart; circle <= circStop; circle+= step ) {
-
-      circ = (double)circle / MAX_STEPS;
-      x = r * cos( M_PI * circ ) + cx;
-      y = r * sin( M_PI * circ ) *
-	cos( 2 * M_PI * rot ) + cy;
-      z = r * sin( M_PI * circ ) *
-	sin( 2 * M_PI * rot ) + cz;
-
-      add_point( points, x, y, z);
-    }
-  }
-}   */ 
 
 void generate_sphere( struct matrix * points, 
 		      double cx, double cy, double cz, double r, 
@@ -391,72 +301,7 @@ void generate_sphere( struct matrix * points,
 void add_torus( struct matrix * points, 
 		double cx, double cy, double cz, double r1, double r2, 
 		int step ) {
-
-  /*struct matrix * temp;
-  int lat, longt;
-  int index;
-  int num_steps;
-  
-  num_steps = MAX_STEPS / step;
-
-  temp = new_matrix( 4, num_steps * num_steps );
-  //generate the points on the torus
-  generate_torus( temp, cx, cy, cz, r1, r2, step );
-  int num_points = temp->lastcol;
-
-  int latStop, longtStop, latStart, longStart;
-  latStart = 0;
-  longStart = 0;
-  latStop = num_steps;
-  longtStop = num_steps;
-  for ( lat = latStart; lat < latStop; lat++ )
-    for ( longt = longStart; longt < longtStop; longt++ ) {
-
-      index = lat * num_steps + longt;
-
-      if ( longt != num_steps-1) {
-	add_polygon( points, temp->m[0][index],
-		     temp->m[1][index],
-		     temp->m[2][index],
-		     temp->m[0][(index+num_steps+1) % num_points],
-		     temp->m[1][(index+num_steps+1) % num_points],
-		     temp->m[2][(index+num_steps+1) % num_points],
-		     temp->m[0][index+1],
-		     temp->m[1][index+1],
-		     temp->m[2][index+1] );
-	add_polygon( points, temp->m[0][index],
-		     temp->m[1][index],
-		     temp->m[2][index],
-		     temp->m[0][(index+num_steps) % num_points],
-		     temp->m[1][(index+num_steps) % num_points],
-		     temp->m[2][(index+num_steps) % num_points],
-		     temp->m[0][(index+num_steps) % num_points + 1],
-		     temp->m[1][(index+num_steps) % num_points + 1],
-		     temp->m[2][(index+num_steps) % num_points + 1]);
-      }
-      else {
-	add_polygon( points, temp->m[0][index],
-		     temp->m[1][index],
-		     temp->m[2][index],
-		     temp->m[0][(index+1) % num_points],
-		     temp->m[1][(index+1) % num_points],
-		     temp->m[2][(index+1) % num_points],
-		     temp->m[0][index+1-num_steps],
-		     temp->m[1][index+1-num_steps],
-		     temp->m[2][index+1-num_steps] );
-	add_polygon( points, temp->m[0][index],
-		     temp->m[1][index],
-		     temp->m[2][index],
-		     temp->m[0][(index+num_steps) % num_points],
-		     temp->m[1][(index+num_steps) % num_points],
-		     temp->m[2][(index+num_steps) % num_points],
-		     temp->m[0][(index+1) % num_points],
-		     temp->m[1][(index+1) % num_points],
-		     temp->m[2][(index+1) % num_points]);
-      }
-
-    }*/
-          struct matrix * temp;
+  struct matrix * temp;
   int lat, longt;
   int index;
   int num_steps;
@@ -477,12 +322,6 @@ void add_torus( struct matrix * points,
       
       index = lat * num_steps + longt;
       
-      /*add_edge( points, temp->m[0][index],
-		temp->m[1][index],
-		temp->m[2][index],
-		temp->m[0][index] + 1,
-		temp->m[1][index] + 1,
-		temp->m[2][index] );*/
       if(lat == latStop-1){
       	int index2 = latStart * num_steps + longt;
       	if(longt == longtStop-1){
@@ -732,11 +571,6 @@ void add_curve( struct matrix *points,
     xcoefs = generate_curve_coefs(x0, x1, x2, x3, HERMITE_MODE);
     ycoefs = generate_curve_coefs(y0, y1, y2, y3, HERMITE_MODE);
   }
-
-  /*
-  printf("a = %lf b = %lf c = %lf d = %lf\n", xcoefs->m[0][0],
-         xcoefs->m[1][0], xcoefs->m[2][0], xcoefs->m[3][0]);
-  */
 
   for (t=step; t <= 1; t+= step) {
     
